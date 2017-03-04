6dee369e4164cb7e1e31b39df945c5dddfff3b6d
geekdada <me@dada.li>
2017-03-04 15:07:57 +0800

    chore: add newrelic

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+if (process.env.NODE_ENV === 'production') {
+  require('newrelic');
+}
+
 require('egg').startCluster({
   baseDir: __dirname,
   port: process.env.PORT || 7001, // default to 7001
