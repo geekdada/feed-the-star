9a6b4c2dc5efee29c6f28cdc07425f9c6e14d5af
Roy Li <me@dada.li>
2019-05-21 10:30:04 +0800

    feat: assert config first

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const assert = require('assert');
 module.exports = app => {
   app.locals.title = '🌟 Feed the Star';
+
+  assert(app.config.site.host);
+  assert(app.config.site.githubClientId);
+  assert(app.config.site.githubClientSecret);
 };
